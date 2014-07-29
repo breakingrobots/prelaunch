3f645ea8a3a0d5e42ffac17597bade52723d8a41
Mike Heijmans <parabuzzle@gmail.com>
2014-07-28 20:25:25 -0700

    ready for initial push

--- server.rb
+++ server.rb
@@ -1,39 +1,40 @@
 require 'sinatra'
 require 'postmark'
 
-enable :sessions
-
-
-get '/' do
-	@notice = session[:notice]
-	erb :index
-end
-
-get '/terms' do
-	File.read(File.join('terms.html'))
-end
-
-get '/terms.html' do
-	File.read(File.join('terms.html'))
-end
-
-get '/privacy' do
-	File.read(File.join('privacy.html'))
-end
-
-get '/privacy.html' do
-	File.read(File.join('privacy.html'))
-end
-
-post '/sendemail' do
-	#your_api_key = ENV['POSTMARK_API_KEY'] ||= '474c993f-fa68-4481-80a4-06f25c8b80f1'
-	#client = Postmark::ApiClient.new(your_api_key)
-	#email = params['email']
-  	#client.deliver(from: 'system@siteclimate.com',
-  	#            to: 'siteclimate@gmail.com',
-  	#            subject: "[new beta email] #{email}",
-  	#            text_body: "Awesome! #{email} is interested in the app! Go you!")
-  	notice = 'Thank you for submitting your email. We will contact you when we are ready for your help.'
-  	session[:notice] = notice
-  	redirect "/", 302
+class SiteClimate < Sinatra::Base
+	enable :sessions
+	
+	get '/' do
+		@notice = session[:notice]
+		erb :index
+	end
+	
+	get '/terms' do
+		File.read(File.join('terms.html'))
+	end
+	
+	get '/terms.html' do
+		File.read(File.join('terms.html'))
+	end
+	
+	get '/privacy' do
+		File.read(File.join('privacy.html'))
+	end
+	
+	get '/privacy.html' do
+		File.read(File.join('privacy.html'))
+	end
+	
+	post '/sendemail' do
+		your_api_key = ENV['POSTMARK_API_KEY']
+		client = Postmark::ApiClient.new(your_api_key)
+		email = params['email']
+	  	client.deliver(from: 'system@siteclimate.com',
+	  	            to: 'siteclimate@gmail.com',
+	  	            subject: "[new beta email] #{email}",
+	  	            text_body: "Awesome! #{email} is interested in the app! Go you!")
+	  	notice = 'Thank you for submitting your email. We will contact you when we are ready for your help.'
+	  	session[:notice] = notice
+	  	redirect "/", 302
+	end
 end	
